106f0376b6ef82105394dd8eb8eaa3cce01d9818
Marselina-you <marselina88@list.ru>
2023-08-15 17:53:28 +0300

    video4 the end
     нужен Google Maps API Key за который нужно платить денежки, поэтому карта и не работает

--- app/js/main.js
+++ app/js/main.js
@@ -165,23 +165,6 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-left'
   }
 });
-let map;
-async function initMap() {
-  //@ts-ignore
-  const {
-    Map
-  } = await google.maps.importLibrary("maps");
-  map = new Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
-  });
-}
-
-//initMap();
-window.initMap = initMap;
 
 /***/ }),
 
--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -15,17 +15,4 @@ const swiper = new Swiper('.swiper', {
 
 
 });
-let map;
 
-async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-
-//initMap();
-window.initMap = initMap;
