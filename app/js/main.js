24f93160a894a048e6edd400000e89c80aaab279
Marselina-you <marselina88@list.ru>
2023-08-17 18:07:00 +0300

    video-5, the end

--- app/js/main.js
+++ app/js/main.js
@@ -163,7 +163,21 @@ const swiper = new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left'
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
   }
+});
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
 });
 
 /***/ }),
--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -13,6 +13,26 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-left',
   },
 
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+
+    }
+  }
+
+
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuMobile.classList.toggle('menu--open');
+
 
 });
 
